bb64135d0ea6fc344d1c7e16a958011269dcb14a
robmadden <rob.madden@bloc.io>
2015-06-16 19:03:23 -0700

    CRUD

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context "attributes" do
-    let(:post) { Post.new(title: "", body: "New Post Body") }
+    let(:post) { Post.new(title: "New Post Title", body: "New Post Body") }
 
     it "should respond to title" do
       expect(post).to respond_to(:title)
     end
+
+    it "should assign title properly" do
+      expect(post.title).to eql "New Post Title"
+    end
     
     it "should respond to body" do
       expect(post).to respond_to(:body)
+    end
+
+    it "should assign body properly" do
+      expect(post.body).to eql "New Post Body"
     end
     
   end
