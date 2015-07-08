f13c7b17b9e2556ccd55b33477b5635ec2292062
robmadden <rob.madden@bloc.io>
2015-07-08 15:34:36 -0700

    User Authentication Continued

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def signin
+  end
+
+  def logout
+    @user = nil
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+    if @user.save
+      flash[:success] = "Welcome to Bloccit!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  get 'sessions/new'
 
   resources :topics do
   	resources :posts, except: [:index]
   end
+
+  get 'new'  => 'users#new'
+  resources :users
+
+  get    'signin'   => 'sessions#new'
+  post   'signin'   => 'sessions#create'
+  delete 'signout'  => 'sessions#destroy'
 
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
