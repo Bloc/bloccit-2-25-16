4c723b50629e2fb12a01c1781663168cc86b3dee
robmadden <rob.madden@bloc.io>
2015-09-02 17:49:20 -0700

    Labels Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903150656) do
+ActiveRecord::Schema.define(version: 20151124003838) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -52,10 +52,18 @@ ActiveRecord::Schema.define(version: 20150903150656) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.integer  "rating_id"
   end
 
+  add_index "posts", ["rating_id"], name: "index_posts_on_rating_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -63,7 +71,10 @@ ActiveRecord::Schema.define(version: 20150903150656) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating_id"
   end
+
+  add_index "topics", ["rating_id"], name: "index_topics_on_rating_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
