6b9b381ab3562f632d2401de6bede5931669c456
Mike Jewett <mikesjewett@gmail.com>
2015-09-03 23:30:28 -0400

    Completed Favoriting without emails

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Post, type: :model do
 
   it { should have_many(:comments) }
   it { should have_many(:votes) }
+  it { should have_many(:favorites) }
   it { should belong_to(:topic) }
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
@@ -72,6 +73,6 @@ RSpec.describe Post, type: :model do
         post.votes.create!(value: -1)
         expect(post.rank).to eq (old_rank - 1)
       end
-    end 
+    end
   end
 end
