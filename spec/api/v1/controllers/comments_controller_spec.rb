fd7bc86a66b1c8a455d5d3318400f4a263c1c190
robmadden <rob.madden@bloc.io>
2015-11-17 20:40:10 -0800

    Assignment Amendments

--- spec/api/v1/controllers/comments_controller_spec.rb
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -1,14 +1,51 @@
-class Api::V1::CommentsController < Api::V1::BaseController
-  before_filter :authenticate_user, except: [:index, :show]
-  before_filter :authorize_user, except: [:index, :show]
+require 'rails_helper'
+include RandomData
 
-  def index
-    comments = Comment.all
-    render json: comments.to_json, status: 200
+RSpec.describe Api::V1::CommentsController, type: :controller do
+  let(:my_user) { create(:user) }
+  let(:my_topic) { create(:topic) }
+  let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
+
+  context "unauthenticated user" do
+    it "GET index returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns http success" do
+      get :show, id: my_comment.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns comment" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_hash['body']).to eq my_comment.body
+    end
   end
 
-  def show
-    comment = Comment.find(params[:id])
-    render json: comment.to_json, status: 200
+  context "unauthorized user" do
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    end
+
+    it "GET index returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns http success" do
+      get :show, id: my_comment.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "GET show returns child comments" do
+      get :show, id: my_comment.id
+      response_hash = JSON.parse response.body
+      expect(response_hash['id']).to eq my_comment.id
+      expect(response_hash['body']).to eq my_comment.body
+    end
   end
 end
